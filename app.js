1bd45fb520935e3a48367e071f04842e9f99b7b4
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-10-31 09:55:45 -0300

    Add true to proxy for Heroku

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.set("trust proxy");
+  app.set("trust proxy", true);
 }
 
 // 1) Set Pug as the template engine
