26d214aff77876ac3afc2fc951e74a751f39ee30
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-27 10:18:28 -0300

    Build bundle to commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6843,7 +6843,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41481" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33745" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
