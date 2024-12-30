8ea21b3fa585b9af7c849846be7e7202100256ce
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-12-30 15:09:46 -0300

    Small change to .pug

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6969,7 +6969,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44905" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46691" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
