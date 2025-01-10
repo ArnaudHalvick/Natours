53c8247154d6c029bb946ec7baa3466859d5df81
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-01-10 14:00:29 -0300

    Small change to edit tour modal and added imgs for test tour

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9493,7 +9493,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43299" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37283" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
