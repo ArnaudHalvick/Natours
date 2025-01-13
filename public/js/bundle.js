531c3fe79a681a117accdacf56219675a7f40f36
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-01-13 15:17:55 -0300

    Small change to CSS to prepare harmonization of CSS

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9590,7 +9590,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40451" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39689" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
