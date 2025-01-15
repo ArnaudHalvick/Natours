74b9fe6f23d246391f4fc52717f1cd4c797ed614
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-01-15 14:55:38 -0300

    feat: implement booking table filters and sorting
    - Add BookingFiltersHandler class for managing filters
    - Implement tour name filtering functionality
    - Add sorting by purchase date, start date, and price
    - Handle empty states when no bookings match filters
    - Update initBookingHandlers to include filter initialization

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6854,6 +6854,16 @@ exports.initBookingHandlers = void 0;
 var _bookingAPI = require("../api/bookingAPI");
 var _alert = require("../utils/alert");
 var _config = require("../config");
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
+function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _regeneratorRuntime() { "use strict"; /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/facebook/regenerator/blob/main/LICENSE */ _regeneratorRuntime = function _regeneratorRuntime() { return e; }; var t, e = {}, r = Object.prototype, n = r.hasOwnProperty, o = Object.defineProperty || function (t, e, r) { t[e] = r.value; }, i = "function" == typeof Symbol ? Symbol : {}, a = i.iterator || "@@iterator", c = i.asyncIterator || "@@asyncIterator", u = i.toStringTag || "@@toStringTag"; function define(t, e, r) { return Object.defineProperty(t, e, { value: r, enumerable: !0, configurable: !0, writable: !0 }), t[e]; } try { define({}, ""); } catch (t) { define = function define(t, e, r) { return t[e] = r; }; } function wrap(t, e, r, n) { var i = e && e.prototype instanceof Generator ? e : Generator, a = Object.create(i.prototype), c = new Context(n || []); return o(a, "_invoke", { value: makeInvokeMethod(t, r, c) }), a; } function tryCatch(t, e, r) { try { return { type: "normal", arg: t.call(e, r) }; } catch (t) { return { type: "throw", arg: t }; } } e.wrap = wrap; var h = "suspendedStart", l = "suspendedYield", f = "executing", s = "completed", y = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} var p = {}; define(p, a, function () { return this; }); var d = Object.getPrototypeOf, v = d && d(d(values([]))); v && v !== r && n.call(v, a) && (p = v); var g = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(p); function defineIteratorMethods(t) { ["next", "throw", "return"].forEach(function (e) { define(t, e, function (t) { return this._invoke(e, t); }); }); } function AsyncIterator(t, e) { function invoke(r, o, i, a) { var c = tryCatch(t[r], t, o); if ("throw" !== c.type) { var u = c.arg, h = u.value; return h && "object" == _typeof(h) && n.call(h, "__await") ? e.resolve(h.__await).then(function (t) { invoke("next", t, i, a); }, function (t) { invoke("throw", t, i, a); }) : e.resolve(h).then(function (t) { u.value = t, i(u); }, function (t) { return invoke("throw", t, i, a); }); } a(c.arg); } var r; o(this, "_invoke", { value: function value(t, n) { function callInvokeWithMethodAndArg() { return new e(function (e, r) { invoke(t, n, e, r); }); } return r = r ? r.then(callInvokeWithMethodAndArg, callInvokeWithMethodAndArg) : callInvokeWithMethodAndArg(); } }); } function makeInvokeMethod(e, r, n) { var o = h; return function (i, a) { if (o === f) throw Error("Generator is already running"); if (o === s) { if ("throw" === i) throw a; return { value: t, done: !0 }; } for (n.method = i, n.arg = a;;) { var c = n.delegate; if (c) { var u = maybeInvokeDelegate(c, n); if (u) { if (u === y) continue; return u; } } if ("next" === n.method) n.sent = n._sent = n.arg;else if ("throw" === n.method) { if (o === h) throw o = s, n.arg; n.dispatchException(n.arg); } else "return" === n.method && n.abrupt("return", n.arg); o = f; var p = tryCatch(e, r, n); if ("normal" === p.type) { if (o = n.done ? s : l, p.arg === y) continue; return { value: p.arg, done: n.done }; } "throw" === p.type && (o = s, n.method = "throw", n.arg = p.arg); } }; } function maybeInvokeDelegate(e, r) { var n = r.method, o = e.iterator[n]; if (o === t) return r.delegate = null, "throw" === n && e.iterator.return && (r.method = "return", r.arg = t, maybeInvokeDelegate(e, r), "throw" === r.method) || "return" !== n && (r.method = "throw", r.arg = new TypeError("The iterator does not provide a '" + n + "' method")), y; var i = tryCatch(o, e.iterator, r.arg); if ("throw" === i.type) return r.method = "throw", r.arg = i.arg, r.delegate = null, y; var a = i.arg; return a ? a.done ? (r[e.resultName] = a.value, r.next = e.nextLoc, "return" !== r.method && (r.method = "next", r.arg = t), r.delegate = null, y) : a : (r.method = "throw", r.arg = new TypeError("iterator result is not an object"), r.delegate = null, y); } function pushTryEntry(t) { var e = { tryLoc: t[0] }; 1 in t && (e.catchLoc = t[1]), 2 in t && (e.finallyLoc = t[2], e.afterLoc = t[3]), this.tryEntries.push(e); } function resetTryEntry(t) { var e = t.completion || {}; e.type = "normal", delete e.arg, t.completion = e; } function Context(t) { this.tryEntries = [{ tryLoc: "root" }], t.forEach(pushTryEntry, this), this.reset(!0); } function values(e) { if (e || "" === e) { var r = e[a]; if (r) return r.call(e); if ("function" == typeof e.next) return e; if (!isNaN(e.length)) { var o = -1, i = function next() { for (; ++o < e.length;) if (n.call(e, o)) return next.value = e[o], next.done = !1, next; return next.value = t, next.done = !0, next; }; return i.next = i; } } throw new TypeError(_typeof(e) + " is not iterable"); } return GeneratorFunction.prototype = GeneratorFunctionPrototype, o(g, "constructor", { value: GeneratorFunctionPrototype, configurable: !0 }), o(GeneratorFunctionPrototype, "constructor", { value: GeneratorFunction, configurable: !0 }), GeneratorFunction.displayName = define(GeneratorFunctionPrototype, u, "GeneratorFunction"), e.isGeneratorFunction = function (t) { var e = "function" == typeof t && t.constructor; return !!e && (e === GeneratorFunction || "GeneratorFunction" === (e.displayName || e.name)); }, e.mark = function (t) { return Object.setPrototypeOf ? Object.setPrototypeOf(t, GeneratorFunctionPrototype) : (t.__proto__ = GeneratorFunctionPrototype, define(t, u, "GeneratorFunction")), t.prototype = Object.create(g), t; }, e.awrap = function (t) { return { __await: t }; }, defineIteratorMethods(AsyncIterator.prototype), define(AsyncIterator.prototype, c, function () { return this; }), e.AsyncIterator = AsyncIterator, e.async = function (t, r, n, o, i) { void 0 === i && (i = Promise); var a = new AsyncIterator(wrap(t, r, n, o), i); return e.isGeneratorFunction(r) ? a : a.next().then(function (t) { return t.done ? t.value : a.next(); }); }, defineIteratorMethods(g), define(g, u, "Generator"), define(g, a, function () { return this; }), define(g, "toString", function () { return "[object Generator]"; }), e.keys = function (t) { var e = Object(t), r = []; for (var n in e) r.push(n); return r.reverse(), function next() { for (; r.length;) { var t = r.pop(); if (t in e) return next.value = t, next.done = !1, next; } return next.done = !0, next; }; }, e.values = values, Context.prototype = { constructor: Context, reset: function reset(e) { if (this.prev = 0, this.next = 0, this.sent = this._sent = t, this.done = !1, this.delegate = null, this.method = "next", this.arg = t, this.tryEntries.forEach(resetTryEntry), !e) for (var r in this) "t" === r.charAt(0) && n.call(this, r) && !isNaN(+r.slice(1)) && (this[r] = t); }, stop: function stop() { this.done = !0; var t = this.tryEntries[0].completion; if ("throw" === t.type) throw t.arg; return this.rval; }, dispatchException: function dispatchException(e) { if (this.done) throw e; var r = this; function handle(n, o) { return a.type = "throw", a.arg = e, r.next = n, o && (r.method = "next", r.arg = t), !!o; } for (var o = this.tryEntries.length - 1; o >= 0; --o) { var i = this.tryEntries[o], a = i.completion; if ("root" === i.tryLoc) return handle("end"); if (i.tryLoc <= this.prev) { var c = n.call(i, "catchLoc"), u = n.call(i, "finallyLoc"); if (c && u) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } else if (c) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); } else { if (!u) throw Error("try statement without catch or finally"); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } } } }, abrupt: function abrupt(t, e) { for (var r = this.tryEntries.length - 1; r >= 0; --r) { var o = this.tryEntries[r]; if (o.tryLoc <= this.prev && n.call(o, "finallyLoc") && this.prev < o.finallyLoc) { var i = o; break; } } i && ("break" === t || "continue" === t) && i.tryLoc <= e && e <= i.finallyLoc && (i = null); var a = i ? i.completion : {}; return a.type = t, a.arg = e, i ? (this.method = "next", this.next = i.finallyLoc, y) : this.complete(a); }, complete: function complete(t, e) { if ("throw" === t.type) throw t.arg; return "break" === t.type || "continue" === t.type ? this.next = t.arg : "return" === t.type ? (this.rval = this.arg = t.arg, this.method = "return", this.next = "end") : "normal" === t.type && e && (this.next = e), y; }, finish: function finish(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.finallyLoc === t) return this.complete(r.completion, r.afterLoc), resetTryEntry(r), y; } }, catch: function _catch(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.tryLoc === t) { var n = r.completion; if ("throw" === n.type) { var o = n.arg; resetTryEntry(r); } return o; } } throw Error("illegal catch attempt"); }, delegateYield: function delegateYield(e, r, n) { return this.delegate = { iterator: values(e), resultName: r, nextLoc: n }, "next" === this.method && (this.arg = t), y; } }, e; }
 function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
@@ -7226,17 +7236,101 @@ var BookingHandler = /*#__PURE__*/function () {
       return handleAddTravelersSubmit;
     }()
   }]);
-}(); // Initialize booking handlers
-var initBookingHandlers = exports.initBookingHandlers = function initBookingHandlers() {
-  // Skip initialization if we're on the management page
-  if (document.querySelector(".user-view__bookings-container")) {
-    return;
+}(); // Add this to your handlers/booking.js file
+var BookingFiltersHandler = /*#__PURE__*/function () {
+  function BookingFiltersHandler() {
+    _classCallCheck(this, BookingFiltersHandler);
+    this.tourFilter = document.getElementById("tourFilter");
+    this.sortBy = document.getElementById("sortBy");
+    this.bookingTableBody = document.getElementById("bookingTableBody");
+    this.originalRows = Array.from(this.bookingTableBody.querySelectorAll("tr"));
+    this.initializeEventListeners();
   }
-  try {
-    new BookingHandler();
-  } catch (error) {
-    console.error("Failed to initialize booking handlers:", error);
-    (0, _alert.showAlert)("error", "Failed to initialize booking system");
+  return _createClass(BookingFiltersHandler, [{
+    key: "initializeEventListeners",
+    value: function initializeEventListeners() {
+      var _this2 = this;
+      if (this.tourFilter) {
+        this.tourFilter.addEventListener("change", function () {
+          return _this2.applyFilters();
+        });
+      }
+      if (this.sortBy) {
+        this.sortBy.addEventListener("change", function () {
+          return _this2.applyFilters();
+        });
+      }
+    }
+  }, {
+    key: "applyFilters",
+    value: function applyFilters() {
+      var _this3 = this;
+      var filteredRows = _toConsumableArray(this.originalRows);
+
+      // Apply tour filter
+      var selectedTour = this.tourFilter.value;
+      if (selectedTour) {
+        filteredRows = filteredRows.filter(function (row) {
+          var _row$querySelector;
+          var tourName = (_row$querySelector = row.querySelector(".tour-name")) === null || _row$querySelector === void 0 ? void 0 : _row$querySelector.textContent;
+          return tourName === selectedTour;
+        });
+      }
+
+      // Apply sorting
+      var _this$sortBy$value$sp = this.sortBy.value.split("-"),
+        _this$sortBy$value$sp2 = _slicedToArray(_this$sortBy$value$sp, 2),
+        sortField = _this$sortBy$value$sp2[0],
+        sortDirection = _this$sortBy$value$sp2[1];
+      filteredRows.sort(function (a, b) {
+        var _a$querySelector, _b$querySelector, _a$querySelector2, _b$querySelector2, _a$querySelector3, _b$querySelector3;
+        var aValue, bValue;
+        switch (sortField) {
+          case "createdAt":
+            aValue = new Date((_a$querySelector = a.querySelector(".td-purchase")) === null || _a$querySelector === void 0 ? void 0 : _a$querySelector.textContent);
+            bValue = new Date((_b$querySelector = b.querySelector(".td-purchase")) === null || _b$querySelector === void 0 ? void 0 : _b$querySelector.textContent);
+            break;
+          case "price":
+            aValue = parseFloat((_a$querySelector2 = a.querySelector(".td-price")) === null || _a$querySelector2 === void 0 ? void 0 : _a$querySelector2.textContent.replace("$", "").replace(",", ""));
+            bValue = parseFloat((_b$querySelector2 = b.querySelector(".td-price")) === null || _b$querySelector2 === void 0 ? void 0 : _b$querySelector2.textContent.replace("$", "").replace(",", ""));
+            break;
+          case "startDate":
+            aValue = new Date((_a$querySelector3 = a.querySelector(".td-start")) === null || _a$querySelector3 === void 0 ? void 0 : _a$querySelector3.textContent);
+            bValue = new Date((_b$querySelector3 = b.querySelector(".td-start")) === null || _b$querySelector3 === void 0 ? void 0 : _b$querySelector3.textContent);
+            break;
+          default:
+            aValue = 0;
+            bValue = 0;
+        }
+        var compareResult = aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
+        return sortDirection === "asc" ? compareResult : -compareResult;
+      });
+
+      // Update the table
+      this.bookingTableBody.innerHTML = "";
+      if (filteredRows.length > 0) {
+        filteredRows.forEach(function (row) {
+          return _this3.bookingTableBody.appendChild(row.cloneNode(true));
+        });
+      } else {
+        var emptyRow = document.createElement("tr");
+        emptyRow.className = "empty-row";
+        emptyRow.innerHTML = "\n        <td colspan=\"7\">\n          <div class=\"empty-message\">\n            <i class=\"fas fa-calendar-times\"></i>\n            <p>No bookings found</p>\n          </div>\n        </td>\n      ";
+        this.bookingTableBody.appendChild(emptyRow);
+      }
+    }
+  }]);
+}(); // Modify the initBookingHandlers function to include the filters handler
+var initBookingHandlers = exports.initBookingHandlers = function initBookingHandlers() {
+  // Initialize booking handlers only on the my-tours page
+  if (window.location.pathname === "/my-tours") {
+    try {
+      new BookingHandler();
+      new BookingFiltersHandler(); // Add this line
+    } catch (error) {
+      console.error("Failed to initialize booking handlers:", error);
+      (0, _alert.showAlert)("error", "Failed to initialize booking system");
+    }
   }
 };
 },{"../api/bookingAPI":"api/bookingAPI.js","../utils/alert":"utils/alert.js","../config":"config.js"}],"api/reviewAPI.js":[function(require,module,exports) {
@@ -10584,7 +10678,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34351" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42963" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- public/js/src/handlers/booking.js
+++ public/js/src/handlers/booking.js
@@ -371,17 +371,110 @@ class BookingHandler {
   }
 }
 
-// Initialize booking handlers
-export const initBookingHandlers = () => {
-  // Skip initialization if we're on the management page
-  if (document.querySelector(".user-view__bookings-container")) {
-    return;
+// Add this to your handlers/booking.js file
+
+class BookingFiltersHandler {
+  constructor() {
+    this.tourFilter = document.getElementById("tourFilter");
+    this.sortBy = document.getElementById("sortBy");
+    this.bookingTableBody = document.getElementById("bookingTableBody");
+    this.originalRows = Array.from(
+      this.bookingTableBody.querySelectorAll("tr"),
+    );
+
+    this.initializeEventListeners();
   }
 
-  try {
-    new BookingHandler();
-  } catch (error) {
-    console.error("Failed to initialize booking handlers:", error);
-    showAlert("error", "Failed to initialize booking system");
+  initializeEventListeners() {
+    if (this.tourFilter) {
+      this.tourFilter.addEventListener("change", () => this.applyFilters());
+    }
+    if (this.sortBy) {
+      this.sortBy.addEventListener("change", () => this.applyFilters());
+    }
+  }
+
+  applyFilters() {
+    let filteredRows = [...this.originalRows];
+
+    // Apply tour filter
+    const selectedTour = this.tourFilter.value;
+    if (selectedTour) {
+      filteredRows = filteredRows.filter(row => {
+        const tourName = row.querySelector(".tour-name")?.textContent;
+        return tourName === selectedTour;
+      });
+    }
+
+    // Apply sorting
+    const [sortField, sortDirection] = this.sortBy.value.split("-");
+    filteredRows.sort((a, b) => {
+      let aValue, bValue;
+
+      switch (sortField) {
+        case "createdAt":
+          aValue = new Date(a.querySelector(".td-purchase")?.textContent);
+          bValue = new Date(b.querySelector(".td-purchase")?.textContent);
+          break;
+        case "price":
+          aValue = parseFloat(
+            a
+              .querySelector(".td-price")
+              ?.textContent.replace("$", "")
+              .replace(",", ""),
+          );
+          bValue = parseFloat(
+            b
+              .querySelector(".td-price")
+              ?.textContent.replace("$", "")
+              .replace(",", ""),
+          );
+          break;
+        case "startDate":
+          aValue = new Date(a.querySelector(".td-start")?.textContent);
+          bValue = new Date(b.querySelector(".td-start")?.textContent);
+          break;
+        default:
+          aValue = 0;
+          bValue = 0;
+      }
+
+      const compareResult = aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
+      return sortDirection === "asc" ? compareResult : -compareResult;
+    });
+
+    // Update the table
+    this.bookingTableBody.innerHTML = "";
+    if (filteredRows.length > 0) {
+      filteredRows.forEach(row =>
+        this.bookingTableBody.appendChild(row.cloneNode(true)),
+      );
+    } else {
+      const emptyRow = document.createElement("tr");
+      emptyRow.className = "empty-row";
+      emptyRow.innerHTML = `
+        <td colspan="7">
+          <div class="empty-message">
+            <i class="fas fa-calendar-times"></i>
+            <p>No bookings found</p>
+          </div>
+        </td>
+      `;
+      this.bookingTableBody.appendChild(emptyRow);
+    }
+  }
+}
+
+// Modify the initBookingHandlers function to include the filters handler
+export const initBookingHandlers = () => {
+  // Initialize booking handlers only on the my-tours page
+  if (window.location.pathname === "/my-tours") {
+    try {
+      new BookingHandler();
+      new BookingFiltersHandler(); // Add this line
+    } catch (error) {
+      console.error("Failed to initialize booking handlers:", error);
+      showAlert("error", "Failed to initialize booking system");
+    }
   }
 };
